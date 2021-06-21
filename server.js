8e2c21db63d65b429e6a60a83b9de1ea39b802a5
VikramTheBlackDragoon <vikramdesmoines@gmail.com>
2021-06-21 09:07:39 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
-const home = require('./routes/home.js');
 
 app.use('/api/genres', genres);
-app.use('/', home);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
